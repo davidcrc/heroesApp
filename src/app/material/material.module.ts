d23337f862f341798f02190f7f6a52ca93841527
davidcrc <david_drcr@hotmail.com>
2021-12-20 16:45:20 -0500

    14-5. Diseño de la pantalla para agregar héroes

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -7,6 +7,8 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
+
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -20,6 +22,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatIconModule,
     MatInputModule,
     MatListModule,
+    MatSelectModule,
     MatSidenavModule,
     MatProgressSpinnerModule,
     MatToolbarModule,
